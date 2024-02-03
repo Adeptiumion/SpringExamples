6df71eb4aa1da9093ff3d6973f2f3315f1512974
Adept <adeptiumion@gmail.com>
2024-02-03 14:10:18 +0700

    definitelyTheLastOneFixResourcesInHibernate

---
+++ src/Examples/16_Hibernate/Resources/Config.java
@@ -0,0 +1,12 @@
+package Resources;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+// Конфиг
+@Configuration
+@ComponentScan("Entity")
+public class Config {
+
+}
